e426a669f379be3e16e08e63b25565d8696602e1
Ivonne <ivonnebenites2015@gmail.com>
2023-11-03 22:18:44 -0500

    Update file

--- schema.sql
+++ schema.sql
@@ -42,3 +42,29 @@ ADD CONSTRAINT fk_owner
 FOREIGN KEY (owner_id)
 REFERENCES owners(id);
 
+/* creating the table named vets */
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name varchar(100),
+    age INT,
+    date_of_graduation DATE
+);
+
+/* creating a join table called specializations */
+CREATE TABLE specializations (
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id),
+    PRIMARY KEY (vet_id, species_id)
+);
+
+/* creating a join table called visits */
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id)
+);
+
+ALTER TABLE visits DROP CONSTRAINT visits_pkey;
+
+ALTER TABLE visits ADD PRIMARY KEY (animal_id, vet_id, visit_date);
