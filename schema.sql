5b43c9a711919065b6bdea238532f0934e459ba1
Jose RM <mojica.raul@gmail.com>
2023-11-06 09:18:21 -0600

    Add visits and owners

--- schema.sql
+++ schema.sql
@@ -71,5 +71,4 @@ ALTER TABLE visits ADD PRIMARY KEY (animal_id, vet_id, visit_date);
 
 /* database performance audit */
 -- Add an email column to your owners table
--- Add an email column to your owners table
 ALTER TABLE owners ADD COLUMN email VARCHAR(120);
