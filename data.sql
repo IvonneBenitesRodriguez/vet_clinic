b1ddca6ddf1ade7f334dbba4f7a10604bc0c639c
Eng. Happi Gilbert <110288182+gilberthappi@users.noreply.github.com>
2023-11-07 00:29:49 +0200

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,4 @@
 /* Populate database with sample data. */
-
 INSERT INTO animals (name,date_of_birth,escape_attemps,neutered,weight_kg)
 VALUES ('Agumon', '2020-02-03',0, true, 10.23),
 ('Gabumon', '2018-11-15',2, true, 8),
